dae8777d06f79f7c65b3d883aa83108dc16ced3d
jazz runner <df.iotinker.git@gmail.com>
2021-04-05 07:21:05 -0400

    add readme, config

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Vaccine Appointment Scanner
+This script checks the macovidvaccines site for sites that have slots open for vaccination appointments. For each clickable signup option, it will also open that page in another browser tab, to get you started on the signup process yourself. The script will also send you a text message using Twilio if there are sites with open appointments.
+
+# Requirements and limitations
+* Selenium Webdriver and Chrome
+* Script uses configparser and argparser
+* It is written only for Chrome browser
+* It is written to use only sms from Twilio
+* Some sites have specific restrictions, for example you must live in a certain community, or you must have a certain career like healthcare worker or educator. This script clicks the signup button for those sites anyway. Make sure you read the restrictions before continuing the signup process, or else you may end up being denied a shot when you show up at the scheduled time!
+
+
+# Usage tips
+python scan_for_appointment.py --zip _zipcode_ --distance _search_dist_
+
+Keep in mind the state guidelines for eligibility. Don't jump the line. This script won't lie for you, but it will make it easier for you to lie. Don't. I wrote this script only because once it _is_ your turn in line, you'll be hard pressed to find an open slot.
+
+I suggest you use python virtual environments to install selenium and twilio libraries and keep this env separate from your default environment. I have used pipenv (Pipfile) but also provided a requirements.txt (this part is untested).
+
+Appointments for vaccinations fill up very quickly - often in less than five minutes. I highly recommend you have cached pharmacy credentials in Chrome: create accounts at CVS, Walgreens, and other sites, to make it easier for you to continue the signup process after this script runs.
+
+The chromedriver only gets closed if there are no slots available. If there are, you'll need to dismiss the window manually before running the script again, otherwise you'll get an invalid argument exception about the data profile path being in use.
+
+## ini file
+The Chromedriver in Selenium will use your profile but you will need to change the default in my-settings.ini. You don't have to use your own profile, but if you don't, there will be no cached credentials so you will be a little slower to grab that appointment slot. Find your *userprofile* by navigating to _chrome://version_ in the address bar and find your Profile Path. *chromedriver* is where you installed the selenium Chromedriver binary.
+
+The notify section should contain your telephone number so that you can send yourself an *sms* text message. I have only tested my US-based telephone number. Don't include this section (or keep the sms value blank) if you don't want text message notification.
+
+## environment
+This script presumes you have set up some environment variables for a Twilio account. The SID and TOKEN represent your account and the token to access the Twilio REST API. The PHONE is the free phone number you can get from Twilio.
+
+# Possible next steps
+* Set this up to run periodically. However, this will also send text messages periodically, so watch out for data message rates.
+* Expand to a variety of selectable notification methods (twitter, email, etc), and don't rely on Twilio.
+* Do a way better job of handling the Xpath elements. This was quickly cobbled together, is inefficient and ugly.
+* Handle the distance setting better; radio buttons changed from 5, 10, 25 at one point to 10, 25, 50 and I had to change code for that. Should be able to dynamically build this list.
+* Allow a way to identify which site(s) should be ignored the next time the script is run. If your search distance pulls up sites close enough to you but that have residency requirements you don't meet, for example, it would be nice to ignore that site the next time the script is run.
+* Support other browsers.
--- scan_for_appointment.py
+++ scan_for_appointment.py
@@ -4,14 +4,22 @@ from selenium.webdriver.common.keys import Keys
 import re
 import argparse
 import configparser
+from twilio.rest import Client
+import os
 
-allowed_dist = [5, 10, 25] # round any distance lower than each to the next one up
+# for notification, use your own free twilio account
+TWILIO_SID = os.environ['TWILIO_SID']
+TWILIO_TOKEN = os.environ['TWILIO_TOKEN']
+TWILIO_PHONE = os.environ['TWILIO_PHONE']
+
+# distances as used by macovidvaccines
+allowed_dist = [10, 25, 50] # round any distance lower than each to the next one up
 any_dist = 9999 # this is considered 'any'
 
 
 """ gather list of hits from macovidvaccines site
   zip = string representing zip code
-  distance = int representing distance value (-1 (=any), 5, 10, 25)
+  distance = int representing distance value (-1 (=any), 10, 25, 50)
 """
 def gather_sites(driver, zip, distance):
     # if distance not valid, make it 'any'
@@ -21,16 +29,37 @@ def gather_sites(driver, zip, distance):
     available_check = driver.find_element_by_name('onlyShowAvailable')
     if not available_check.is_selected():
         available_check.click()
-    zip_code = driver.find_element_by_name('zip-code')
-    dist_25 = driver.find_element_by_xpath(radio_elem_xp)
-    zip_code.send_keys(zip)
-    dist_25.click()
+    # clear previous, otherwise we may have two zipcodes in there
+    driver.find_element_by_name('zip-code').send_keys(Keys.CONTROL + "a")
+    driver.find_element_by_name('zip-code').send_keys(zip)
+
+    dist_opt = driver.find_element_by_xpath(radio_elem_xp)
+    dist_opt.click()
+
     apply_filt = driver.find_element_by_xpath("//button[contains(.,'Apply Filters')]")
     apply_filt.click()
 
+""" send notification to user there are slots available
+  notify_config = config object from ConfigParser
+  message = string message
+"""
+def send_notify(notify_config, message):
+    # verify notify specification and that sms isn't empty(ish)
+    if notify_config != None and 'sms' in notify_config and len(notify_config['sms'])>4:
+        client = Client(TWILIO_SID, TWILIO_TOKEN)
+        message = client.messages.create(
+            to=notify_config['sms'],
+            from_=TWILIO_PHONE,
+            body=message
+        )
+    else:
+        print("No valid notification spec")
+
+
+# Arguments for zip and distance
 parser = argparse.ArgumentParser(description="Find available covid vaccine appointments in Massachusetts and open Chrome tabs to sign up for each")
 parser.add_argument('--zip', default='02134', type=ascii, help='five-digit zip code to search for')
-parser.add_argument('--distance', default=5, type=int, help='search distance (5, 10, 25, or -1 for any distance)')
+parser.add_argument('--distance', default=5, type=int, help='search distance (10, 25, 50, or -1 for any distance)')
 args=parser.parse_args()
 # args.zip
 # args.distance
@@ -43,26 +72,38 @@ else:
             args.distance = i
             break
 
+# Config file for Chrome user information and SMS information
+config = configparser.ConfigParser()
+config.read('my-settings.ini')
+chromespec = None if 'CHROMESPEC' not in config else config['CHROMESPEC']
+notifyspec = None if 'NOTIFY' not in config else config['NOTIFY']
 
-driver = webdriver.Chrome()
+if chromespec == None:
+    driver = webdriver.Chrome()
+else:
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir={}".format(chromespec['userprofile']))
+    driver = webdriver.Chrome(executable_path=chromespec['chromedriver'], options=options)
 driver.get('https://www.macovidvaccines.com/')
 gather_sites(driver, args.zip, args.distance)
 
+# mm/yy/dd: n slots
 pattern = re.compile(r".*\d{1,2}\/\d{1,2}\/\d{2}: (\d{1,4}) slot.*")
 #clickable_signup = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary"
-
 #wrap_list = driver.find_element_by_xpath('//div[@id="progress"]/div[@class="loadedContent"]/div[@role="list"]')
-#print(len(wrap_list))
 items = driver.find_elements_by_xpath('//div[@role="listitem"]')
-known_count = 0
-unk_count = 0
-known_list = []
+known_list = []   # sites that display known number of slots
 known_names = []
-unknown_list = []
+unknown_list = [] # sites (like CVS) that do not display number of slots
+known_count = 0   # sum of all slots over all known sites
+unk_count = 0     # sum of all "unknown" sites
 for item in items:
     for aline in item.text.splitlines():
         match = pattern.match(aline)
         if match:
+            print(match.group(0))
+            print(match.group(1))
+#            print(match.group(2))
             known_count += int(match.group(1))
             known_list.append(item)
             # the name of the location is identified by MuiCardHeader-title span class
@@ -72,18 +113,19 @@ for item in items:
             unknown_list.append(item)
 
 print("--------------------------------------")
-print("Found {} slots-locations".format(str(len(known_list))), end=": ")
+print("Found {} slots-locations".format(str(known_count)), end=": ")
 print(*known_names, sep=", ")
 print("Found {} locations with TBD slot count".format(str(len(unknown_list))))
 if len(known_list)+len(unknown_list):
-    for site in known_list + unknown_list: #.extend(unknown_list):
+    for site in known_list + unknown_list:
         try:
-#            site.find_element_by_xpath('.//a[@class="{}"]'.format(clickable_signup)).click()
             # signup button distinguished by class MuiButton-root
             site.find_element_by_xpath('.//a[contains(@class, "MuiButton-root")]').click()
         except:
             print("fail to find sign-up button for {}".format(site.text))
     print("Subtabs have been opened")
+    send_notify(notifyspec,
+        "Found {} slots-locations and {} TBD slots. Check chromedriver.".format(str(known_count), str(len(unknown_list))))
 else:
     driver.close()
 print("--------------------------------------")
